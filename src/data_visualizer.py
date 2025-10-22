33c84871a01ef744bf65f43f8b22730363fded13
6007574 <david.martinez@latam.com>
2025-10-21 20:14:40 -0500

    Add complementary figures to complete data analysis

--- src/data_visualizer.py
+++ src/data_visualizer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import DataFrame
 import warnings
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,6 +12,47 @@ plt.rcParams["figure.figsize"] = (8, 5)
 warnings.filterwarnings('ignore',category=FutureWarning)
 warnings.filterwarnings('ignore',category=UserWarning)
 
+def save_success_rate_by_2dim(df:DataFrame, col1:str, col2:str, destination_path:str):
+    df['y_numeric'] = df['y'].map({'yes':1, 'no':0})
+    pivot = df.pivot_table(values='y_numeric', index=col1, columns=col2, aggfunc='mean')
+    df.drop("y_numeric", axis=1, inplace = True)
+    plt.figure(figsize=(7, 5))
+    sns.heatmap(pivot, annot=True, fmt=".2f", cmap="YlGnBu")
+    plt.title(f"Tasa de éxito por {col1} y {col2}")
+    plt.ylabel(col1)
+    plt.xlabel(col2)
+    plt.savefig(f"{destination_path}/success_rate_by_{col1}_and_{col2}.png")
+    plt.close()
+
+def save_target_proprotion_by_job(df:DataFrame, destination_path:str):
+    df['y_numeric'] = df['y'].map({'yes': 1, 'no': 0})
+
+    plt.figure(figsize=(12,6))
+    sns.barplot(
+        data=df, 
+        x='job', 
+        y='y_numeric', 
+        estimator='mean', 
+        order=df['job'].value_counts().index
+    )
+    df.drop("y_numeric", axis=1, inplace = True)
+    plt.xticks(rotation=45)
+    plt.title("Tasa de éxito de la campaña por ocupación")
+    plt.ylabel("Proporción de 'sí'")
+    plt.xlabel("Ocupación")
+    plt.savefig(f"{destination_path}/success_rate_by_job.png")
+    plt.close()
+
+def save_distribution_by_column(df:DataFrame, col:str, destination_path:str):
+    plt.figure(figsize=(8, 4))
+    order = df[col].value_counts().index
+    sns.countplot(y=col, data=df, order=order)
+    plt.title(f"Distribución de {col}")
+    plt.xlabel("Cantidad")
+    plt.ylabel(col)
+    plt.savefig(f"{destination_path}/{col}_distribution.png")
+    plt.close()
+
 def visualize_data(
     source_path:str = "data/raw/bank-additional-full.csv",
     destination_path:str = "docs/figures/"
@@ -20,7 +62,7 @@ def visualize_data(
 
     df = pd.read_csv(source_path, sep=';')
 
-    # Figure # 1
+    # Figure # 1 Distribución del target
     plt.figure(figsize=(8, 6))
     sns.countplot(x="y", data=df)
     plt.title("Distribución de la variable objetivo (suscripción al depósito)")
@@ -29,18 +71,10 @@ def visualize_data(
     plt.savefig(f"{destination_path}/target_distribution.png")
     plt.close()
 
-    # Figure # 2
-    plt.figure(figsize=(8, 4))
-    col = "education"
-    order = df[col].value_counts().index
-    sns.countplot(y=col, data=df, order=order)
-    plt.title(f"Distribución de {col}")
-    plt.xlabel("Cantidad")
-    plt.ylabel(col)
-    plt.savefig(f"{destination_path}/education_distribution.png")
-    plt.close()
+    # Figure # 2 distirbución de educación
+    save_distribution_by_column(df, "education", destination_path)
 
-    # Figure # 3
+    # Figure # 3 Matriz de correlación
     num_df = df.select_dtypes(include=['float64', 'int64'])
     corr = num_df.corr()
     plt.figure(figsize=(10, 8))
@@ -49,16 +83,17 @@ def visualize_data(
     plt.savefig(f"{destination_path}/correlation_matrix.png")
     plt.close()
 
-    # Figure # 4
-    plt.figure(figsize=(8, 4))
-    col = "job"
-    order = df[col].value_counts().index
-    sns.countplot(y=col, data=df, order=order)
-    plt.title(f"Distribución de {col}")
-    plt.xlabel("Cantidad")
-    plt.ylabel(col)
-    plt.savefig(f"{destination_path}/job_distribution.png")
-    plt.close()
+    # Figure # 4 distirbución de ocupación
+    save_distribution_by_column(df, "job", destination_path)
+
+    # Figure # 5 Tasa de exito por educación
+    save_target_proprotion_by_job(df, destination_path)
+
+    # Figure # 6 Tasa de exito por educación y ocupación
+    save_success_rate_by_2dim(df, "education", "job", destination_path)
+
+    # Figure # 7 Tasa de exito por medio de contacto y mes
+    save_success_rate_by_2dim(df, "contact", "month", destination_path)
 
 
 if __name__ == "__main__":

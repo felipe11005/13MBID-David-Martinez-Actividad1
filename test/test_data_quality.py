13a71dc1d27d02231b7c3daa6a9363f36206eb68
6007574 <david.martinez@latam.com>
2025-10-26 10:08:26 -0500

    stop tracking figure by git, and start tracking by dvc

--- test/test_data_quality.py
+++ test/test_data_quality.py
@@ -6,12 +6,22 @@ import pytest
 
 @pytest.fixture
 def datos_banco():
+    """Fixture para cargar los datos del banco desde un archivo CSV.
+    Returns:
+        pd.DataFrame: DataFrame que contiene los datos del banco.
+    """
     df = pd.read_csv("data/raw/bank-additional-full.csv", sep=';')
     return df
 
 
 def test_esquema(datos_banco):
+    """Test de esquema para el DataFrame de datos_banco.
+
+    Args:
+        datos_banco (pd.DataFrame): DataFrame que contiene los datos del banco.
+    """
     df = datos_banco
+    print(len(df.columns))
     esquema = DataFrameSchema({
         "age": Column(int, nullable=False),
         "job": Column(str, nullable=False),
@@ -20,8 +30,48 @@ def test_esquema(datos_banco):
         "default": Column(str, nullable=True),
         "housing": Column(str, nullable=False),
         "loan": Column(str, nullable=False),
+        "contact": Column(str, nullable=False),
+        "month": Column(str, nullable=False),
+        "day_of_week": Column(str, nullable=False),
+        "duration": Column(int, nullable=False),
+        "campaign": Column(int, nullable=False),
+        "pdays": Column(int, nullable=False),
+        "previous": Column(int, nullable=False),
+        "poutcome": Column(str, nullable=False),
+        "emp.var.rate": Column(float, nullable=False),
+        "cons.price.idx": Column(float, nullable=False),
+        "cons.conf.idx": Column(float, nullable=False),
+        "euribor3m": Column(float, nullable=False),
+        "nr.employed": Column(float, nullable=False),
         "y": Column(str, nullable=False)
-        # TODO: completar el resto de columnas
     })
 
     esquema.validate(df)
+
+def test_basico(datos_banco):
+    """Test básico para verificar que el DataFrame de datos_banco no está vacío
+    y contiene las columnas esperadas.
+
+    Args:
+        datos_banco (pd.DataFrame): DataFrame que contiene los datos del banco.
+    """
+    df = datos_banco
+    # Verificar que el DataFrame no está vacío
+    assert not df.empty, "El DataFrame está vacío." 
+    # Verificar nulos
+    assert df.isnull().sum().sum() == 0, "El DataFrame contiene valores nulos."
+    # Verificar cantidad de columnas
+    assert df.shape[1] == 21, f"El DataFrame debería tener 21 columnas, pero tiene {df.shape[1]}."
+
+
+if __name__ == "__main__":
+    try:
+        test_esquema(datos_banco())
+        test_basico(datos_banco())
+        print("Todos los tests pasaron exitosamente.")
+        with open("docs/test_results/test_results.txt", "w") as f:
+            f.write("Todos los tests pasaron exitosamente.\n")
+    except AssertionError as e:
+        print(f"Test fallido: {e}")
+        with open("docs/test_results/test_results.txt", "w") as f:
+            f.write(f"Test fallido: {e}\n")
